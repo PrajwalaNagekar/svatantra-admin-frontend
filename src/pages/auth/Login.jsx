8bcb3b242bf98aafceab792f370bfbc5a34228d7
PrajwalaNagekar <prajwalanagekar@gmail.com>
2025-07-03 10:37:09 +0530

    login image changed

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import loginImage from '../../assets/images/image.png';
+import loginImage from '../../assets/images/logos/login-img.jpeg';
 import PopupModal from '../../components/popupModal/PopupModal';
 import { adminLogin } from '../../api';
 
